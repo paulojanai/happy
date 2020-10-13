149cec92480177a143d65ede6f893cec2b1cc020
paulojanai <paulojanai.m.s@gmail.com>
2020-10-13 11:26:16 -0300

    style landing add

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+
+import "./sass/app.css";
+import "./sass/screens/landing.css";
 
 function App() {
-	return (
-		<div>
-			<h1>Happy</h1>
-		</div>
-	);
+  return (
+    <div className="page-landing">
+      <h1>Happy</h1>
+    </div>
+  );
 }
 
 export default App;
