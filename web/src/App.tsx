880589226c302f22fd5149deca7cdead1db19572
paulojanai <paulojanai.m.s@gmail.com>
2020-10-13 13:55:02 -0300

    finish screen home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { ArrowRight } from "react-feather";
 
 import "./sass/app.css";
 import "./sass/screens/landing.css";
 
+import logo from "./assets/logo.svg";
+
 function App() {
   return (
     <div className="page-landing">
-      <h1>Happy</h1>
+      <div className="content-wrapper">
+        <img src={logo} alt="happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Canindé</strong>
+          <span>Ceará</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <ArrowRight className="enter-icon" size={28} />
+        </a>
+      </div>
     </div>
   );
 }
---
+++ web/src/styles/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  backgroundColor: "#EBF2F5",
+  shapes01: "#FFFFFF",
+  blueLight: "#96FEFF",
+  blue: "#15C3D6",
+  blueShadow: "#12AFCB",
+  cyan: "#00c7c7",
+  yellow: "#FFD666",
+  greenLight: "#3EE08F",
+  green: "#37C77F",
+  brown: "#8D734B",
+};
